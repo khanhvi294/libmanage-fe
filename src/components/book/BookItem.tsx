3811f8e6f89ff50de5b5f2e7f650fae6c67d8ef7
Vi Nguyen <khanhvi2941@gmail.com>
2023-07-27 00:07:56 +0700

    build UI BookItem

--- src/components/book/BookItem.tsx
+++ src/components/book/BookItem.tsx
@@ -1,17 +1,27 @@
-// import Image from "next/image";
-// import React from "react";
-// import img from "/image.jpg";
+import Image from "next/image";
+import React from "react";
+import img from "../../../public/test.jpg";
 
 type Props = {};
 
 const BookItem = (props: Props) => {
   return (
     <div className="w-[230px] h-[294px] bg-white flex flex-col p-2">
-      {/* <Image src={img} alt="book image" width={100} height={24} priority /> */}
-      <div className="h-24">
-        <p className="font-base">Gieo Trồng Hạnh Phúc (Tái Bản 2021)</p>
-        <p>Thích Nhất Hạnh</p>
-        <p>self help</p>
+      <div className="w-[182px] m-auto relative">
+        <Image src={img} alt="book image" className="w-full h-auto" priority />
+      </div>
+      <div className="h-24 p-1 mt-1">
+        <p className="font-base  text-sm leading-4">
+          Gieo Trồng Hạnh Phúc (Tái Bản 2021)
+        </p>
+        <p className="text-xs italic mt-2">
+          <span className="font-bold">Tác giả:</span> Thích Nhất Hạnh
+        </p>
+        <p className="text-xs mt-2">
+          <span className="border-[1px] text-cyan-400 border-cyan-400 rounded-lg py-0.5 px-1">
+            Tiểu thuyết
+          </span>
+        </p>
       </div>
     </div>
   );
